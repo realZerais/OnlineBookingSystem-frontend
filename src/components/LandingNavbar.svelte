434bcdea938780e6a28c3a67dc60f2fa35fa5b6b
zerais <davesiarez18@gmail.com>
2023-11-09 09:56:25 +0800

    routing initialized

--- src/components/LandingNavbar.svelte
+++ src/components/LandingNavbar.svelte
@@ -1,7 +1,4 @@
 <script>
-    import LoginForm from "./LoginForm.svelte";
-    import SignUpForm from "./SignUpForm.svelte";
-
     let isLoginComponentVisible = false;
     let isSignUpComponentVisible = false;
 
@@ -25,24 +22,19 @@
         <li><a href="#" class="text-xl font-bold hover:text-green-400">Contact</a></li>
     </ul>
     <ul class="flex justify-start items-center gap-7">
-        <button on:click={toggleLoginComponent} class="cursor-pointer transition-all bg-buttonColor text-white px-6 py-2 rounded-lg
+        <a href="/login" on:click={toggleLoginComponent} class="cursor-pointer transition-all bg-buttonColor text-white px-6 py-2 rounded-lg
         border-buttonColorAccent border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
         active:border-b-[2px] active:brightness-90 active:translate-y-[2px]" >
         Log In
-        </button>
-        <button on:click={toggleSignUpComponent} class="cursor-pointer transition-all bg-buttonColor text-white px-6 py-2 rounded-lg
+    </a>
+        <a href="/register" on:click={toggleSignUpComponent} class="cursor-pointer transition-all bg-buttonColor text-white px-6 py-2 rounded-lg
         border-buttonColorAccent marker:border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
         active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
         Sign Up
-        </button>
+    </a>
     </ul>
 </nav>
 
-{#if isLoginComponentVisible}
-<LoginForm/>
-{:else if isSignUpComponentVisible}
-<SignUpForm/>
-{/if}
 
 
 
