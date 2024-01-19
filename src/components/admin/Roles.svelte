5680093656750b3323a04e2ab2a3821325d2a770
zerais <davesiarez18@gmail.com>
2024-01-19 22:35:24 +0800

    manage rolestable

--- src/components/admin/Roles.svelte
+++ src/components/admin/Roles.svelte
@@ -12,7 +12,7 @@
   })
 </script>
 
-<div class="flex flex-col justify-start items-center p-2 ml-10 mt-4 gap-4">
+<div class="flex flex-col justify-start items-center p-2 ml-10 mt-4 gap-5">
 
   <div class="header flex items-center justify-center h-[10vh] w-[95%] rounded-sm  bg-slate-100 text-4xl">
     <h1>MANAGE ROLES</h1>
@@ -20,17 +20,18 @@
 
   
 
-  <div class="flex flex-col  items-start justify-start p-5  w-[95%] rounded-xl  bg-white text-4xl gap-5">
+  <div class="flex flex-col  items-start justify-start p-5  w-[95%] rounded-xl  bg-white text-4xl gap-2 ">
    
-    <div class="flex items-center justify-between w-[100%] gap-2 p-6">
-      <h1 class=" text-left">USERS:</h1>
-      <button on:click={editRole}>save changes</button>
-    </div>
+
   
+    <div class="flex items-center justify-around w-[100%] gap-2 mb-5 ">
+      <h1>USERNAME</h1>
+      <h1>ROLE</h1>
+    </div>
 
     
-    <div class="flex items-center justify-around h-[20vh] w-[100%] bg-amber-200">
-      <p>username: {username} </p>
+    <div class="flex items-center justify-around h-[20vh] w-[100%] rounded-xl bg-main text-white gap-6 relative">
+      <p >{username} </p>
 
       <div>
         <label for="cars">Role:</label>
@@ -40,12 +41,14 @@
           <option value="technician">Mercedes</option>
           <option value="staff">Audi</option>
         </select>
+
+        <button class="bg-accent p-4 text-lg rounded-xl absolute" disabled on:click={editRole}>save changes</button>
       </div>
       
     </div>
  
-    <div class="flex items-center justify-around h-[20vh] w-[100%] bg-amber-200">
-      <p>username: {username} </p>
+    <div class="flex items-center justify-around h-[20vh] w-[100%] rounded-xl bg-main text-white ">
+      <p >{username} </p>
 
       <div>
         <label for="cars">Role:</label>
@@ -59,8 +62,8 @@
       
     </div>
 
-    <div class="flex items-center justify-around h-[20vh] w-[100%] bg-amber-200">
-      <p>username: {username} </p>
+    <div class="flex items-center justify-around h-[20vh] w-[100%] rounded-xl bg-main text-white gap-6 ">
+      <p >{username} </p>
 
       <div>
         <label for="cars">Role:</label>
@@ -74,8 +77,8 @@
       
     </div>
 
-    <div class="flex items-center justify-around h-[20vh] w-[100%] bg-amber-200">
-      <p>username: {username} </p>
+    <div class="flex items-center justify-around h-[20vh] w-[100%] rounded-xl bg-main text-white gap-6 ">
+      <p >{username} </p>
 
       <div>
         <label for="cars">Role:</label>
@@ -89,8 +92,8 @@
       
     </div>
 
-    <div class="flex items-center justify-around h-[20vh] w-[100%] bg-amber-200">
-      <p>username: {username} </p>
+    <div class="flex items-center justify-around h-[20vh] w-[100%] rounded-xl bg-main text-white gap-6 ">
+      <p >{username} </p>
 
       <div>
         <label for="cars">Role:</label>
@@ -103,6 +106,8 @@
       </div>
       
     </div>
+      
+    
     
     
 
